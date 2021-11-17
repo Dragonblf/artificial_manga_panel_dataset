67e2202d67010c6fb38b6d70d96d4cb427140846
Luis Felipe Murguia Ramos <felipelfmr1@gmail.com>
2021-11-17 00:11:18 -0600

    [edited] We ajust padding and some sizes using 4 multiplos

--- preprocesing/config_file.py
+++ preprocesing/config_file.py
@@ -1,12 +1,12 @@
 # **Page rendering**
-page_width = 1700
+page_width = 1600
 page_height = 2400
 
 page_size = (page_width, page_height)
 
 output_format = ".png"
 
-boundary_width = 10
+boundary_width = 16
 
 # **Font coverage**
 # How many characters of the dataset should the font files support
@@ -35,43 +35,42 @@ vertical_horizontal_ratios = {
 }
 
 # Panel transform chance
-
-panel_transform_chance = 0.90
+panel_transform_chance = 0.96
 
 # Panel shrinking
 
-panel_shrink_amount = -25
+panel_shrink_amount = -24
 
 # Panel removal
-
-panel_removal_chance = 0.01
+panel_removal_chance = 0.012
 panel_removal_max = 2
 
 # Background adding
-background_add_chance = 0.01
+background_add_chance = 0.012
 
 # **Speech bubbles**
+bubble_content_padding = 24
 max_speech_bubbles_per_panel = 3
-bubble_to_panel_area_max_ratio = 0.4
-bubble_mask_x_increase = 15
-bubble_mask_y_increase = 15
+bubble_to_panel_area_max_ratio = 0.48
+bubble_mask_x_increase = 16
+bubble_mask_y_increase = 16
 min_font_size = 24
 max_font_size = 48
 
 # *Transformations*
 
 # Slicing
-double_slice_chance = 0.25
-slice_minimum_panel_area = 0.2
-center_side_ratio = 0.7
+double_slice_chance = 0.24
+slice_minimum_panel_area = 0.24
+center_side_ratio = 0.72
 
 # Box transforms
-box_transform_panel_chance = 0.1
-panel_box_trapezoid_ratio = 0.5
+box_transform_panel_chance = 0.16
+panel_box_trapezoid_ratio = 0.48
 
 # How much at most should the trapezoid/rhombus start from
 # as a ratio of the smallest panel's width or height
-trapezoid_movement_limit = 50
-rhombus_movement_limit = 50
+trapezoid_movement_limit = 48
+rhombus_movement_limit = 48
 
-full_page_movement_proportion_limit = 25
+full_page_movement_proportion_limit = 24
